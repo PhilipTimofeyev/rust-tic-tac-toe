4f37b308e150f75764aa80b2c527e3e76c84a739
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-03-07 09:29:10 -0800

    Add struct Player and organize

--- src/main.rs
+++ src/main.rs
@@ -12,64 +12,61 @@ const WINNING_SQUARES: [[u8; 3]; 8] = [
     [3, 5, 7],
 ];
 
+struct Player {
+    name: String,
+    marker: String,
+}
 enum Action {
     Replay,
     Quit,
 }
 
 fn main() {
+    welcome_message();
+
     loop {
         let mut board = initialize_board();
-        let mut current_marker = String::from("X");
         let mut turn: u8 = 0;
-        welcome_message();
+
+        let player1 = Player {
+            name: String::from("Player 1"),
+            marker: String::from("X"),
+        };
+        let player2 = Player {
+            name: String::from("Player 2"),
+            marker: String::from("O"),
+        };
+
+        let mut players = [player1, player2];
+
         draw_board(&board);
 
         loop {
             clear_screen();
 
-            player_turn(&mut board, &current_marker);
+            player_turn(&mut board, &players[0]);
             draw_board(&board);
 
             turn += 1;
-            
-            if check_if_winner(&board, &current_marker, turn) {
+
+            if check_if_winner(&board, &players[0], turn) {
                 break;
-            } else if check_if_draw(turn) {                
-                break
+            } else if check_if_draw(turn) {
+                break;
             }
-            
-            current_marker = set_marker(current_marker);
+
+            players.reverse();
         }
+
         let player_action = get_player_action();
         match player_action {
             Action::Replay => {
                 println!("Starting new game");
                 continue;
-            },
+            }
             Action::Quit => {
                 println!("Thanks for playing!");
-                break
-            },
-        }
-    }
-}
-
-fn get_player_action() -> Action {
-    println!("Play again? Enter y for yes or n for no:");
-
-    loop {
-        let mut response = String::new();
-
-        io::stdin()
-            .read_line(&mut response)
-            .expect("Failed to read line");
-
-        match response.as_str().trim() {
-            "y" => return Action::Replay,
-            "n" => return Action:: Quit,
-            _ => {
-                println!("Please enter y or n:")
+                break;
             }
         }
     }
@@ -82,76 +79,44 @@ fn welcome_message() {
     println!("");
 }
 
-fn check_if_draw(turn: u8) -> bool {
-    if turn == 9 {
-        println!("Draw!");
-        true
-    } else {
-        false
-    }
-}
-
-
 fn clear_screen() {
     print!("{}[2J", 27 as char);
 }
 
-fn place_marker(board: &mut HashMap<u8, String>, player_move: u8, current_marker: &String) {
-    board.insert(player_move, current_marker.to_string());
-}
-
-fn player_turn(board: &mut HashMap<u8, String>, current_marker: &String) {
-    let player_move = get_player_move(&board);
-
-    place_marker(board, player_move, &current_marker);
-}
-
 fn initialize_board() -> HashMap<u8, String> {
     let mut board = HashMap::new();
-
     for i in (1..=9) {
         board.insert(i, i.to_string());
     }
-
     board
 }
 
-fn check_if_winner(board: &HashMap<u8, String>, current_marker: &String, turn: u8) -> bool {
-    if turn < 5 {return false}
+fn draw_board(board: &HashMap<u8, String>) {
+    let a = board.get(&1).unwrap();
+    let b = board.get(&2).unwrap();
+    let c = board.get(&3).unwrap();
 
-    for row in WINNING_SQUARES {
-        let mut count: u8 = 0;
+    let d = board.get(&4).unwrap();
+    let e = board.get(&5).unwrap();
+    let f = board.get(&6).unwrap();
 
-        for square in row {
-            match board.get(&square) {
-                Some(i) => {
-                    if i == current_marker {
-                        count += 1
-                    }
-                }
-                None => (),
-            };
-            if count == 3 {
-                println!("{} is the winner!", current_marker);
-                return true;
-            }
-        }
-    }
-    false
+    let g = board.get(&7).unwrap();
+    let h = board.get(&8).unwrap();
+    let i = board.get(&9).unwrap();
+
+    println!("-------------");
+    println!("| {} | {} | {} |", a, b, c);
+    println!("-------------");
+    println!("| {} | {} | {} |", d, e, f);
+    println!("-------------");
+    println!("| {} | {} | {} |", g, h, i);
+    println!("-------------");
 }
 
-fn set_marker(marker: String) -> String {
-    if marker == "X" {
-        String::from("O")
-    } else {
-        String::from("X")
-    }
-}
+fn player_turn(board: &mut HashMap<u8, String>, current_player: &Player) {
+    let player_move = get_player_move(&board);
 
-enum Marker {
-    X,
-    O,
-    Empty,
+    place_marker(board, player_move, current_player);
 }
 
 fn get_player_move(board: &HashMap<u8, String>) -> u8 {
@@ -180,24 +145,61 @@ fn get_player_move(board: &HashMap<u8, String>) -> u8 {
     }
 }
 
-fn draw_board(board: &HashMap<u8, String>) {
-    let a = board.get(&1).unwrap();
-    let b = board.get(&2).unwrap();
-    let c = board.get(&3).unwrap();
+fn place_marker(board: &mut HashMap<u8, String>, player_move: u8, current_player: &Player) {
+    board.insert(player_move, current_player.marker.to_owned());
+}
 
-    let d = board.get(&4).unwrap();
-    let e = board.get(&5).unwrap();
-    let f = board.get(&6).unwrap();
+fn check_if_winner(board: &HashMap<u8, String>, current_player: &Player, turn: u8) -> bool {
+    if turn < 5 {
+        return false;
+    }
 
-    let g = board.get(&7).unwrap();
-    let h = board.get(&8).unwrap();
-    let i = board.get(&9).unwrap();
+    for row in WINNING_SQUARES {
+        let mut count: u8 = 0;
 
-    println!("-------------");
-    println!("| {} | {} | {} |", a, b, c);
-    println!("-------------");
-    println!("| {} | {} | {} |", d, e, f);
-    println!("-------------");
-    println!("| {} | {} | {} |", g, h, i);
-    println!("-------------");
+        for square in row {
+            match board.get(&square) {
+                Some(i) => {
+                    if *i == current_player.marker {
+                        count += 1
+                    }
+                }
+                None => (),
+            };
+            if count == 3 {
+                println!("{} is the winner!", current_player.name);
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn check_if_draw(turn: u8) -> bool {
+    if turn == 9 {
+        println!("Draw!");
+        true
+    } else {
+        false
+    }
+}
+
+fn get_player_action() -> Action {
+    println!("Play again? Enter y for yes or n for no:");
+
+    loop {
+        let mut response = String::new();
+
+        io::stdin()
+            .read_line(&mut response)
+            .expect("Failed to read line");
+
+        match response.as_str().trim() {
+            "y" => return Action::Replay,
+            "n" => return Action::Quit,
+            _ => {
+                println!("Please enter y or n:")
+            }
+        }
+    }
 }
