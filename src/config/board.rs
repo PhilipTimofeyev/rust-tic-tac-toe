2919008e5a75559d786124a2f14ee059ad202a92
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-03-10 23:39:42 -0700

    Add testing for board and update

--- src/config/board.rs
+++ src/config/board.rs
@@ -53,8 +53,9 @@ impl Board {
     }
 
     pub fn place_marker(&mut self, player_move: u8, current_player: &Player) {
-        self.squares
-            .insert(player_move, current_player.marker.to_owned());
+        if let Some(square) = self.squares.get_mut(&player_move) {
+            *square = current_player.marker.to_string()
+        }
     }
 
     pub fn add_turn(&mut self) {
@@ -84,6 +85,112 @@ impl Board {
     }
 
     pub fn check_if_draw(&self) -> bool {
-        self.turn == 9
+        self.turn >= 9
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn setup_board() {
+        let board = Board::new();
+
+        assert_eq!(board.turn, 0); // Starts with 0 turns
+
+        let num_of_squares = board.squares.iter().count();
+        assert_eq!(num_of_squares, 9) // Contains 9 squares
+    }
+
+    #[test]
+    fn adding_turn() {
+        let mut board = Board::new();
+
+        board.add_turn();
+        assert_eq!(board.turn, 1);
+    }
+
+    #[test]
+    fn placing_marker() {
+        let mut board = Board::new();
+        let player_1 = Player{name: String::from("Player 1"), marker: String::from("X")};
+        let player_2 = Player{name: String::from("Player 2"), marker: String::from("O")};
+
+        board.place_marker(1, &player_1);
+        assert_eq!(board.squares.get(&1).unwrap(), "X");
+        
+        board.place_marker(9, &player_2);
+        assert_eq!(board.squares.get(&9).unwrap(), "O");
+        
+        board.place_marker(12, &player_1); //Returns None if square outside hash range
+        assert_eq!(board.squares.get(&12), None);
+    }
+
+    #[test]
+    fn verify_draw() {
+        let mut board = Board::new();
+
+        board.turn = 8;
+        assert!(!board.check_if_draw());
+        
+        board.turn = 9;
+        assert!(board.check_if_draw());
+    }
+
+    #[test]
+    fn verify_row_winning_squares() {
+        let mut board = Board::new();
+        let player_1 = Player{name: String::from("Player 1"), marker: String::from("X")};
+        board.turn = 5; // Prevent automatic false from too few turns
+        
+        let mut result = board.check_if_winner(&player_1);
+        assert!(!result);
+
+        // Check row
+        board.squares.insert(1, "X".to_string());
+        board.squares.insert(2, "X".to_string());
+        board.squares.insert(3, "X".to_string());
+
+        result = board.check_if_winner(&player_1);
+
+        println!("{:#?}", board.squares);
+        assert!(result)
+    }
+
+    #[test]
+    fn verify_column_winning_squares() {
+        let mut board = Board::new();
+        let player_1 = Player{name: String::from("Player 1"), marker: String::from("X")};
+        board.turn = 5; // Prevent automatic false from too few turns
+
+        // Check column
+        board.squares.insert(1, "X".to_string());
+        board.squares.insert(4, "X".to_string());
+        board.squares.insert(7, "X".to_string());
+
+        let result = board.check_if_winner(&player_1);
+
+        println!("{:#?}", board.squares);
+        assert!(result)
+    }
+
+    #[test]
+    fn verify_diagonal_winning_squares() {
+        let mut board = Board::new();
+        let player_1 = Player{name: String::from("Player 1"), marker: String::from("X")};
+        board.turn = 5; // Prevent automatic false from too few turns
+
+        // Check diagonal
+        board.squares.insert(1, "X".to_string());
+        board.squares.insert(5, "X".to_string());
+        board.squares.insert(9, "X".to_string());
+
+        let result = board.check_if_winner(&player_1);
+
+        println!("{:#?}", board.squares);
+        assert!(result)
+    }
+
 }
